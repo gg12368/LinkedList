8b33e0a864571bba71d8d8af8a8838658b87aeb1
gg12368 <56186550+gg12368@users.noreply.github.com>
2019-10-15 22:31:34 +0800

    Create Linked_list_segmentation.java

---
+++ Linked_list_segmentation.java
@@ -0,0 +1,24 @@
+6.链表分割：编写代码，以给定值x为基准将链表分割成两部分，所有小于x的结点排在大于或等于x的结点之前。
+给定一个链表的头指针 ListNode* pHead，请返回重新排列后的链表的头指针。注意：分割以后保持原来的数据顺序不变。
+
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode before_head = new ListNode(0);
+        ListNode before = before_head;
+        ListNode after_head = new ListNode(0);
+        ListNode after = after_head;
+        while (head != null) {
+            if (head.val < x) {
+                before.next = head;
+                before = before.next;
+            } else {
+                after.next = head;
+                after = after.next;
+            }
+            head = head.next;
+        }
+        after.next = null;
+        before.next = after_head.next;
+        return before_head.next;
+    }
+}
